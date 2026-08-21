5e3dce1007d91de6384972c324adef1ac58da37b
Rodrigo Costa <rodrigo_costa86@hotmail.com>
2026-08-21 11:39:35 -0300

    Adiciona persistência do tema claro e escuro

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ let botaoModo = document.querySelector("#modo-escuro");
 
 let body = document.querySelector("body");
 
+let temaSalvo = localStorage.getItem("tema");
+
+if (temaSalvo === "modo-escuro") {
+    body.classList.add("modo-escuro")
+}
+
 botaoModo.addEventListener("click", mudarModo);
 
 
@@ -11,8 +17,10 @@ function mudarModo() {
 
     if (body.classList.contains("modo-escuro")) {
         botaoModo.textContent = "☀️";
+        localStorage.setItem("tema", "modo-escuro");
     } else {
         botaoModo.textContent = "🌙";
+        localStorage.setItem("tema", "modo-claro")
     }
 
 }
